b54fcdff828935638a806604d0f5b3fa389292a5
Jordan Pratt <jpratt@mtroyal.ca>
2023-02-06 18:33:33 +0000

    Sync

--- htdocs/1-simple-endpoint/api/continents.php
+++ htdocs/1-simple-endpoint/api/continents.php
@@ -18,4 +18,14 @@ $query = <<<QUERY
         continents
 QUERY;
 
-var_dump($db->run($query)->fetchAll());
+$query_result = $db->run($query)->fetchAll();
+// var_dump($query_result); // ❓❓ If we try this before the header() calls, boom. Why? 
+
+$resp = json_encode($query_result, JSON_FORCE_OBJECT);
+
+header("HTTP/1.1 200 OK");
+header("Content-Type: application/json");
+
+echo ($resp);
+
+// ⚠️ I'd validate your JSON using something like https://jsonformatter.curiousconcept.com
